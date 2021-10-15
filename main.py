2a62d02cf3847f9dc66b1635c383d751cbc72a75
Shawn Ringler <ringlersm@gmail.com>
2021-10-15 17:25:29 -0600

    new trained data

--- main.py
+++ main.py
@@ -6,13 +6,13 @@
 
 from QLearning import QLearning
 
-RL = QLearning(10000,
+RL = QLearning(1000,
                debug_on=False,
                visuals_on_while_training=False,
-               load_on=True,
-               save_on=False,
-               file_name="Higher_failure-q-table.json",
+               load_on=False,
+               save_on=True,
+               file_name="high_reward_high_pain-q-table.json",
                show_score_plot=True,
-               training_on=False)
+               training_on=True)
 
 RL.run_optimal_game(n_times=10)
