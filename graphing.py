c9ac53b6f1ba09063033d98cc88d1564561bbb72
Frank Torres <iamfrankietorres@gmail.com>
2021-10-26 13:54:40 -0600

    created learning_loop_multiple_agents_create_graph
    -moved the code in main that graphs multiple agents optimal scores into a function called "learning_loop_multiple_agents_create_graph" in graphing.py
    
    -Planning on cleaning things up a bit more if possible and commenting the code

--- graphing.py
+++ graphing.py
@@ -5,6 +5,61 @@ import numpy as np
 
 from QLearning import QLearning
 
+def learning_loop_multiple_agents_create_graph(episodes, agents, optimals):
+    """
+    Runs the learning loop to create average score data on multiple agents
+    and plots the average of all of the agent averages.
+
+    Args: 
+        episodes: Number of episodes to train the agent(s)
+        agents: Number of agents to train
+        optimals: Number of optimal runs (no exploration) per episode
+
+    Returns:
+        Returns nothing for now but outputs a matplotlib graph which 
+        can then be saved.
+    """
+    #time0 = time.time()
+
+    avgs_list = []
+
+    for i in range(0, agents):
+        print("Agent " + str(i) + " of " + str(agents))
+        agent = QLearning(episodes,
+                debug_on=False,
+                visuals_on_while_training=False,
+                load_on=False,
+                save_on=False,
+                file_name="",
+                show_score_plot=False,
+                training_on=False)
+
+        agent_scores = agent.learning_loop_create_data(optimals)
+        avgs_list.append(agent_scores)
+
+    #time1 = time.time()
+    #print(time1-time0)
+
+    ep_count = []
+    for i in range(0, episodes):
+        ep_count.append(i+1)
+
+    final_avgs = []
+    for i in range(0, episodes):
+        current_list = []
+        for j in avgs_list:
+            current_list.append(j[i])
+        final_avgs.append(stat.mean(current_list))
+
+    plt.scatter(ep_count, final_avgs)
+    plt.xlabel("Episodes Trained")
+    plt.ylabel("Average Score")
+    plt.title("Average Scores over Training Episodes")
+    #z = np.polyfit(ep_count, final_avgs, 2)
+    #p = np.polyval(z, ep_count)
+    #plt.plot(ep_count, p, "r--")
+    plt.show()
+
 def graph_performance():
     """
     This function iteratively creates a new agent for every increment in episode amount
@@ -53,5 +108,3 @@ def graph_performance():
     plt.ylabel('Average Score')
     plt.xlabel('Episodes Trained')
     plt.show()
-
-graph_performance()
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import numpy as np
 import statistics as stat
 import time
 from QLearning import QLearning
+from graphing import learning_loop_multiple_agents_create_graph
 
 """RL = QLearning(3000,
                debug_on=False,
@@ -25,48 +26,4 @@ from QLearning import QLearning
 
 #RL.learning_loop_create_graphs(5)
 
-time0 = time.time()
-
-episodes = 2000
-agents = 100
-optimals = 20
-
-avgs_list = []
-
-for i in range(0, agents):
-    print("Agent " + str(i) + " of " + str(agents))
-    agent = QLearning(episodes,
-               debug_on=False,
-               visuals_on_while_training=False,
-               load_on=False,
-               save_on=False,
-               file_name="",
-               show_score_plot=False,
-               training_on=False)
-
-    agent_scores = agent.learning_loop_create_data(optimals)
-    avgs_list.append(agent_scores)
-
-time1 = time.time()
-
-print(time1-time0)
-
-ep_count = []
-for i in range(0, episodes):
-    ep_count.append(i+1)
-
-final_avgs = []
-for i in range(0, episodes):
-    current_list = []
-    for j in avgs_list:
-        current_list.append(j[i])
-    final_avgs.append(stat.mean(current_list))
-
-plt.scatter(ep_count, final_avgs)
-plt.xlabel("Episodes Trained")
-plt.ylabel("Average Score")
-plt.title("Average Scores over Training Episodes")
-z = np.polyfit(ep_count, final_avgs, 2)
-p = np.polyval(z, ep_count)
-plt.plot(ep_count, p, "r--")
-plt.show()
+learning_loop_multiple_agents_create_graph(100, 1, 10)
