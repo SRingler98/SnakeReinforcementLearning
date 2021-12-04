4c6f2fc85ec839d4d637fabb1b3e33b3e3779479
Shawn Ringler <ringlersm@gmail.com>
2021-12-04 14:57:45 -0700

    added example at the bottom

--- DQNClasses.py
+++ DQNClasses.py
@@ -602,3 +602,22 @@ class DQNLearning:
                     return True
 
         return False
+
+# example of calling DQNClasses for learning
+# print("Creating model")
+# env = SnakeEnv()
+# temp_learn = DQNLearning(env=env,
+#                          target_name=str(str(target[0]) + "," + str(target[1])),
+#                          episode_count=10,
+#                          min_batch_size=50,
+#                          max_batch_size=-1,
+#                          load_model=False,
+#                          fit_on_step=10,
+#                          train=True,
+#                          save_model=False,
+#                          show_graphs=False)
+#
+# print("Training model")
+# temp_agent = temp_learn.train(debug=False,
+#                               # replay_buffer_data=replay_data[target_index]
+#                               )
